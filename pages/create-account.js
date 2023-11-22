132c753da757c82be2f084e3f8749104aadf3f30
AshaniImalsha <ashaniimalsha26@gmail.com>
2023-11-22 21:47:40 +0530

    task/update registration popup

--- pages/create-account.js
+++ pages/create-account.js
@@ -3,15 +3,15 @@ import { useState } from 'react';
 import Link from 'next/link';
 
 const RegisterPage = () => {
-  const [selectedUsers, setSelectedUsers] = useState([]);
+  const [selectedUsers, setSelectedUsers] = useState();
 
   const handleUserSelect = (userId) => {
-    if (selectedUsers.includes(userId)) {
+    if (selectedUsers==userId) {
       // If user is already selected, remove them
       setSelectedUsers(selectedUsers.filter((id) => id !== userId));
     } else {
       // If user is not selected, add them
-      setSelectedUsers([...selectedUsers, userId]);
+      setSelectedUsers(userId);
     }
   };
 
@@ -27,33 +27,33 @@ const RegisterPage = () => {
         <h2 className="text-2xl font-semibold mb-4">Who are you?</h2>
 
         <div className="mb-4">
-          <label className="block text-gray-700 font-bold mb-2">Select your roal to register</label>
+          <label className="block text-gray-700 font-bold mb-2">Select your role to register</label>
           <div className="grid grid-cols-2 gap-4">
             {/* User 1 */}
             <div
               onClick={() => handleUserSelect(1)}
               className={`cursor-pointer p-4 border rounded ${
-                selectedUsers.includes(1) ? 'bg-blue-500 text-white' : 'bg-gray-300'
+                selectedUsers==1 ? 'bg-purple-500 text-white' : 'bg-gray-300'
               }`}
             >
-              User 1
+              Register as Candidate
             </div>
 
             {/* User 2 */}
             <div
               onClick={() => handleUserSelect(2)}
               className={`cursor-pointer p-4 border rounded ${
-                selectedUsers.includes(2) ? 'bg-blue-500 text-white' : 'bg-gray-300'
+                selectedUsers==2 ? 'bg-purple-500 text-white' : 'bg-gray-300'
               }`}
             >
-              User 2
+              Register as HR Manager
             </div>
           </div>
         </div>
 
         <button
           onClick={handleRegistration}
-          className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-700"
+          className="w-full bg-purple-500 text-white p-2 rounded hover:bg-purple-700"
         >
           Register
         </button>
@@ -61,7 +61,7 @@ const RegisterPage = () => {
         <p className="mt-4 text-sm">
           Already have an account?{' '}
           <Link href="/sign-in">
-            <a className="text-blue-500">Login</a>
+            <a className="text-purple-500">Login</a>
           </Link>
         </p>
       </div>
