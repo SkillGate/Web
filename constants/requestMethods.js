c73070caaacc673f519926e517a21e71196eccd9
YohanNayanajith <yohannayanajith40@gmail.com>
2024-04-04 02:18:01 +0530

    task/change deploy urls

--- constants/requestMethods.js
+++ constants/requestMethods.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
 const LOCAL_AUTH_BASE_URL = "http://localhost:5000/api/v1";
-const PROD_AUTH_BASE_URL = "https://skillgate-auth-api.onrender.com/api/v1";
+const PROD_AUTH_BASE_URL = "https://vercel-auth-api.vercel.app/api/v1";
 
 const LOCAL_JOB_BASE_URL = "http://localhost:5001/api/v1";
-const PROD_JOB_BASE_URL = "https://skillgate-job-api.onrender.com/api/v1";
+const PROD_JOB_BASE_URL = "https://vercel-job-api.vercel.app/api/v1";
+
+const LOCAL_CONFIG_BASE_URL = "http://localhost:5001/api/v1";
+const PROD_CONFIG_BASE_URL = "https://config-api-ochre.vercel.app/api/v1";
 
 export const publicAuthRequest = axios.create({
   baseURL: PROD_AUTH_BASE_URL,
@@ -16,6 +19,14 @@ export const publicAuthRequest = axios.create({
 
 export const publicJobRequest = axios.create({
   baseURL: PROD_JOB_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+    "access-control-allow-origin": "*",
+  },
+});
+
+export const publicConfigRequest = axios.create({
+  baseURL: PROD_CONFIG_BASE_URL,
   headers: {
     "Content-Type": "application/json",
     "access-control-allow-origin": "*",
