1ed199feaeca54f7c8df423ed05411501171c9f7
YohanNayanajith <yohannayanajith40@gmail.com>
2024-04-14 00:52:42 +0530

    task/add new skills

--- components/candidate-persona/models/skills-model.js
+++ components/candidate-persona/models/skills-model.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { IoMdClose } from "react-icons/io";
 import { useState } from "react";
 import { useUiContext } from "../../../contexts/UiContext";
@@ -6,6 +6,7 @@ import { UpdateUser } from "../../../apiCalls/userApiCalls";
 import ModelPopup from "../../common/ModelPopup";
 import Loader from "../../common/Loader";
 import { getSkill } from "../../../apiCalls/skillApiCalls";
+import { IoMdAdd } from "react-icons/io";
 
 export const SkillsPopup = ({ user, onClose, onChange }) => {
   const [skills, setSkills] = useState([]);
@@ -17,22 +18,37 @@ export const SkillsPopup = ({ user, onClose, onChange }) => {
   const [loading, setLoading] = useState(false);
   const [skillLoading, setSkillLoading] = useState(false);
 
+  useEffect(() => {
+    const addExistingSkills = () => {
+      if (user && user.skills && user.skills.length !== 0) {
+        setSkills([]);
+        user.skills.map((skill) => {
+          setSkills((prevSkills) => [...user.skills]);
+        });
+      }
+    };
+    addExistingSkills();
+  }, []);
+
   const toggleModal = () => {
     setIsModalVisible(!isModalVisible);
   };
 
   // There is no submit form
-  const onSubmit = async (data) => {
-    console.log(data);
+  const handleAddNewSkill = async () => {
+    console.log(skills);
 
-    if (data.skills !== undefined) {
+    if (skills && skills.length !== 0) {
       setLoading(true);
+      const actualData = {
+        skills: skills,
+      };
       try {
         const {
           data: userData,
           loading,
           error,
-        } = await UpdateUser(user?._id, user?.accessToken, data);
+        } = await UpdateUser(user?._id, user?.accessToken, actualData);
         console.log(userData);
         setLoading(loading);
         if (!userData || userData.length === 0) {
@@ -60,30 +76,10 @@ export const SkillsPopup = ({ user, onClose, onChange }) => {
   const handleAddSkill = async () => {
     console.log(newSkill);
     if (newSkill.trim() !== "") {
-      try {
-        const {
-          data: skillData,
-          loading,
-          error,
-        } = await getSkill(newSkill, user?.accessToken);
-        console.log(skillData);
-        setSkillLoading(loading);
-        if (!skillData || skillData.length === 0) {
-          setIsModalVisible(true);
-        } else {
-          // setIsModalVisibleSuccess(true);
-          skillData.accessToken = user.accessToken;
-          setSkillsUrls((prevSkillsUrls) => [...prevSkillsUrls, skillData]);
-        }
-      } catch (error) {
-        setSkillLoading(false);
-        console.error("Error in onSubmit:", error);
-      }
       setSkills((prevSkills) => [...prevSkills, newSkill]);
       setNewSkill("");
     }
     console.log(skills);
-    console.log(skillUrls);
   };
 
   const removeSkill = (skillToRemove) => {
@@ -114,7 +110,7 @@ export const SkillsPopup = ({ user, onClose, onChange }) => {
           <IoMdClose />
         </button>
         <h2 className="text-xl font-bold mb-8">Skills</h2>
-        <div className="mb-10">
+        <div className="flex mb-10 gap-4">
           <input
             type="text"
             className="outline-none h-8 border border-slate-300  dark:border-hover-color bg-main dark:bg-dark-main rounded-md px-[0.8rem] w-full text-base focus:!border-primary"
@@ -123,6 +119,9 @@ export const SkillsPopup = ({ user, onClose, onChange }) => {
             onChange={(e) => setNewSkill(e.target.value)}
             onKeyPress={handleKeyPress}
           />
+          <button type="click" onClick={handleAddNewSkill}>
+            <IoMdAdd size={20} className="text-primary"></IoMdAdd>
+          </button>
         </div>
         <div className="flex flex-wrap gap-2">
           {skills.map((skill, index) => (
