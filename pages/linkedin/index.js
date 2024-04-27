43a2e3dd6078db0f5d9d59f786410f1d85af0540
AshaniImalsha <ashaniimalsha26@gmail.com>
2024-04-27 22:16:16 +0530

    Updated linkedin page

--- pages/linkedin/index.js
+++ pages/linkedin/index.js
@@ -4,27 +4,37 @@ import { skilldata, recommendationsdata } from "../../data/linkedinData";
 
 const BlogInfo = () => {
 
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = (skill_name) => {
+        setIsHovered(skill_name);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
+
     const firstRec = recommendationsdata[0];
     const [currentRecommendation, setCurrentRecommendation] = useState(firstRec);
     const [currentIndex, setCurrentIndex] = useState(0);
 
     function moveCard(position) {
         const numberOfObjects = recommendationsdata.length
-        if(position === "left"){
-            if(currentIndex === 0){
-                setCurrentRecommendation(recommendationsdata[numberOfObjects-1]);
-                setCurrentIndex(numberOfObjects-1);
-            }else {
-                setCurrentRecommendation(recommendationsdata[currentIndex-1]);
-                setCurrentIndex(currentIndex-1);
+        if (position === "left") {
+            if (currentIndex === 0) {
+                setCurrentRecommendation(recommendationsdata[numberOfObjects - 1]);
+                setCurrentIndex(numberOfObjects - 1);
+            } else {
+                setCurrentRecommendation(recommendationsdata[currentIndex - 1]);
+                setCurrentIndex(currentIndex - 1);
             }
-        }else if(position === "right"){
-            if(currentIndex === numberOfObjects-1){
+        } else if (position === "right") {
+            if (currentIndex === numberOfObjects - 1) {
                 setCurrentRecommendation(recommendationsdata[0]);
                 setCurrentIndex(0);
-            }else {
-                setCurrentRecommendation(recommendationsdata[currentIndex+1]);
-                setCurrentIndex(currentIndex+1);
+            } else {
+                setCurrentRecommendation(recommendationsdata[currentIndex + 1]);
+                setCurrentIndex(currentIndex + 1);
             }
         }
     }
@@ -35,21 +45,32 @@ const BlogInfo = () => {
             <div className="padding-container mb-5">
                 <Back url={"/shortlist/1"} />
             </div>
-            <div className="container mx-auto flex flex-wrap items-center text-center my-4">
-                <div className="mb-10">
-                    <h2 className="font-bold text-lg mb-10">Skills</h2>
-                    <div className="flex flex-wrap mt-5">
+            <div className="container mx-auto flex flex-wrap items-center text-start my-4">
+                <div className="mb-10 w-full">
+                    <h2 className="font-bold text-lg mb-10 text-center">Skills</h2>
+                    <div className="max-w-5xl mx-auto relative flex flex-wrap mt-5 shadow-md bg-white rounded-lg p-5">
                         {skilldata.map(skill => (
-                            <div key={skill.skill_name} className="w-full sm:w-1/2 lg:w-1/3 p-2">
+                            <div
+                                key={skill.skill_name}
+                                className="w-full sm:w-1/2 lg:w-1/4 pl-5 mb-5 hover:bg-purple-500 hover:text-white rounded-lg p-2"
+                                onMouseEnter={() => handleMouseEnter(skill.skill_name)}
+                                onMouseLeave={handleMouseLeave}
+                            >
                                 <h3>{skill.skill_name}</h3>
+                                {isHovered === skill.skill_name && (
+                                    <div className="absolute bg-gray-600 text-gray-100 rounded-lg p-2 shadow-md">
+                                        <h1>Skill Utilized Environments</h1>
+                                        <p>{skill.utilize}</p>
+                                    </div>
+                                )}
                             </div>
                         ))}
                     </div>
                 </div>
                 <div className="mb-10 w-full">
-                    <h2 className="font-bold text-lg mb-10">Recommendations</h2>
+                    <h2 className="font-bold text-lg mb-10 text-center">Recommendations</h2>
                     <div className="mt-5">
-                        <div class="max-w-3xl mx-auto relative overflow-hidden">
+                        <div class="max-w-5xl mx-auto relative overflow-hidden">
                             <div class="relative">
                                 <div class="testimonial-card flex transition-transform duration-300 ease-in-out justify-center items-center">
                                     <div class="w-full bg-white rounded-lg m-1 p-12 shadow-md text-center">
@@ -75,5 +96,7 @@ const BlogInfo = () => {
         </div>
     );
 }
+
+
 
 export default BlogInfo;
