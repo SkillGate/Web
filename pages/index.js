3bea812d3be61d6ac20ad41fe6efd115df1e3b33
YohanNayanajith <yohannayanajith40@gmail.com>
2024-05-06 20:05:23 +0530

    fix/logout bug

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,15 @@
-import { server } from "../config";
-import useFetch from "./api/useFetch";
-import About from "../components/landing/About";
 import Advertisement from "../components/landing/Advertisement";
 import Contact from "../components/landing/Contact";
 import HeroSection from "../components/landing/HeroSection";
 import Services from "../components/landing/Services";
 import Testimonials from "../components/landing/Testimonials";
 import AboutUsSection from "../components/landing/AboutUsSection";
+import { useEffect } from "react";
 
 const Home = () => {
-  const { data: jobs, loading } = useFetch(`${server}/api/jobs`);
-
+  useEffect(() => {
+    window.location.href = "/Web";
+  });
   return (
     <div className="overflow-hidden">
       <div>
