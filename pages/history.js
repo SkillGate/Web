c6d6205922db8eed11562accec4e2c0d68338a3b
Roshan Senevirathne <roshaaanrulz@gmail.com>
2024-05-06 10:27:32 +0530

    HR history page done

--- pages/history.js
+++ pages/history.js
@@ -1,7 +1,6 @@
-import Applied from "../components/history/Applied";
-import Interviewing from "../components/history/Interviewing";
-import Offers from "../components/history/Offers";
-import Saved from "../components/history/Saved";
+import Closed from "../components/history/Closed";
+import Opened from "../components/history/Opened";
+
 
 const History = () => {
   return (
@@ -11,17 +10,17 @@ const History = () => {
         <button className="btn btn-primary">manage job post</button>
       </div>
       <div className="mt-5 flex flex-wrap gap-8">
-        <div className="flex-1 basis-[16rem]">
+        {/* <div className="flex-1 basis-[16rem]">
           <Applied />
         </div>
         <div className="flex-1 basis-[16rem]">
           <Saved />
+        </div> */}
+        <div className="flex-1 basis-[16rem]">
+          <Opened />
         </div>
         <div className="flex-1 basis-[16rem]">
-          <Offers />
-        </div>
-        <div className="flex-1 basis-[16rem]">
-          <Interviewing />
+          <Closed />
         </div>
       </div>
     </div>
